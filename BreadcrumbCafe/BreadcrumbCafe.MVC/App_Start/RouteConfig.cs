dba1ac0c3532a615b42117c0e5acd347bb81f1b7
Pongsakorn Thanopassakul <Pongsakorn.Thanopassakul@bravo.aero>
2015-12-05 17:36:08 +0700

    Commit - Home and Portfolio.

--- BreadcrumbCafe/BreadcrumbCafe.MVC/App_Start/RouteConfig.cs
+++ BreadcrumbCafe/BreadcrumbCafe.MVC/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace BreadcrumbCafe.MVC
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapRoute(
+				name: "Default_WithoutAction",
+				url: "{controller}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+
+			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
 				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
