6c4b8ab475f79b5be2b8a1bfc013ee6b582985ba
Pongsakorn Thanopassakul <Pongsakorn.Thanopassakul@bravo.aero>
2015-12-27 17:40:11 +0700

    Commit - Fix wrong month for countdown.

--- BreadcrumbCafe/BreadcrumbCafe.MVC/assets/js/pages/page_coming_soon.js
+++ BreadcrumbCafe/BreadcrumbCafe.MVC/assets/js/pages/page_coming_soon.js
@@ -6,7 +6,7 @@ var PageComingSoon = function () {
         initPageComingSoon: function () {
 			//var newYear = new Date(); 
         	//newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1); 
-			var nextRelease = new Date(2016, 1, 28);
+			var nextRelease = new Date(2016, 0, 28);
 			$('#defaultCountdown').countdown({ until: nextRelease })
         }
 
