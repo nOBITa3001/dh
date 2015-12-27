2ee2facd1745e7f07e51ac2075989895703491bb
Pongsakorn Thanopassakul <Pongsakorn.Thanopassakul@bravo.aero>
2015-12-27 12:57:46 +0700

    Commit - page scroll.

--- BreadcrumbCafe/BreadcrumbCafe.MVC/assets/js/app.js
+++ BreadcrumbCafe/BreadcrumbCafe.MVC/assets/js/app.js
@@ -16,7 +16,18 @@ var App = function () {
             else {
                 jQuery(".header-fixed .header-sticky").removeClass("header-fixed-shrink");
             }
-        });
+         });
+
+    	//jQuery for page scrolling feature - requires jQuery Easing plugin
+         $(function () {
+         	$('.page-scroll a').bind('click', function (event) {
+         		var $anchor = $(this);
+         		$('html, body').stop().animate({
+         			scrollTop: $($anchor.attr('href')).offset().top
+         		}, 1500, 'easeInOutExpo');
+         		event.preventDefault();
+         	});
+         });
     }
 
     //Header Mega Menu
